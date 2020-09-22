e5766c1399b42cba76d7bb071d2d21b49f41d379
Arvindh12 <arvindhashok1112@gmail.com>
2020-09-22 23:23:44 +0530

    Pagination

--- src/Components/Stories.jsx
+++ src/Components/Stories.jsx
@@ -1,9 +1,46 @@
 import React, { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import NewsCard from "./NewsCard";
 
 function Stories({ storyId, storyType }) {
   const [allStrories, setAllStrories] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handleNext = () => {
+    let storiesList = [];
+    storyId
+      .filter((i, index) => ( (currentPage+1)*10 <= index && index < (currentPage+1)*10 + 10 ) )
+      .forEach((item) => {
+        storiesList.push(
+          fetch(
+            `https://hacker-news.firebaseio.com/v0/item/${item}.json`
+          ).then((res) => res.json())
+        );
+      });
+    Promise.all(storiesList).then((result) => {
+      console.log(result);
+      setAllStrories(result);
+      setCurrentPage(currentPage+1)
+    });
+  }
+
+  const handlePrev = () => {
+    let storiesList = [];
+    storyId
+      .filter((i, index) => ( (currentPage-1)*10 <= index && index < (currentPage-1)*10 + 10 ) )
+      .forEach((item) => {
+        storiesList.push(
+          fetch(
+            `https://hacker-news.firebaseio.com/v0/item/${item}.json`
+          ).then((res) => res.json())
+        );
+      });
+    Promise.all(storiesList).then((result) => {
+      console.log(result);
+      setAllStrories(result);
+      setCurrentPage(currentPage-1)
+    });
+}
 
   useEffect(() => {
     let storiesList = [];
@@ -36,6 +73,12 @@ function Stories({ storyId, storyType }) {
           </Col>
         </Row>
       ))}
+        <Row className="my-3">
+          <Col>
+          <Button className="mx-3" onClick={handlePrev} disabled={currentPage === 0} >Prev</Button>
+          <Button className="mx-3" onClick={handleNext} disabled={currentPage >= storyId.length/10}  >Next</Button>
+          </Col>
+        </Row>
     </Container>
   );
 }
